5cced0c81216ed1549c566f6145218d999eac666
poojar222 <85876968+poojar222@users.noreply.github.com>
2025-01-03 10:59:44 +0530

    Update Tiger.js

--- src/Tiger.js
+++ src/Tiger.js
@@ -2,7 +2,7 @@ import React from 'react';
 const Tiger = () => {
     return (
         <div>
-        <img src = "tiger.jpeg" alt=""/> 
+        <img src = "/tiger.jpeg" alt=""/> 
         <h1> I am tiger </h1>
         </div>
     );
