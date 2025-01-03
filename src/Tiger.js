b293c4856d8c9fcc84c4e604c86dfae4e1c8eb26
pooja <pooja.raghu@zohocorp.com>
2025-01-03 17:26:16 +0530

    test

--- src/Tiger.js
+++ src/Tiger.js
@@ -2,8 +2,8 @@ import React from 'react';
 const Tiger = () => {
     return (
         <div>
-        <img src = "./img/tiger.jpeg" alt="tiger"/> 
-        <h1> I am tiger </h1>
+        <img src = "img/tiger.jpeg" alt="tiger"/> 
+        <h1> I am tiger the great </h1>
         </div>
     );
 };
